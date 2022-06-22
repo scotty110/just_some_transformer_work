7ec43ac318b177cb89a8c1e5f01a586172fd19f1
Scott <none@none.com>
2022-06-21 19:38:13 -0700

    updates to dataloader

--- ai4code/__main__.py
+++ ai4code/__main__.py
@@ -1,14 +1,27 @@
 import torch
 from os.path import join
-from ai4code.data.dataset import load
-from ai4code.data.dataloader import consecutive_cells
+#from ai4code.data.dataset import load
+#from ai4code.data.dataloader import consecutive_cells
+from ai4code.data.dataloader import get_dataloaders 
 
-data_dir='/home/squirt/Documents/AI4Code'
-#load(data_dir)
+#from ai4code.model import transformer
 
-# Data loader
-pk_file = join(data_dir, 'embeddings.pkl') 
-ds = consecutive_cells(pk_file)
-ds[0]
-print('Done')
+if __name__ == '__main__':
+    data_dir='/home/squirt/Documents/AI4Code'
+    #load(data_dir)
+
+    # Data loader
+    # Memory issues, do lazy loading
+    emb_file = join(data_dir, 'embeddings_fp16.pkl') 
+    key_file = join(data_dir, 'embeddings_keys.pkl')
+   
+    t_dl, e_dl = get_dataloaders(emb_file, key_file)
+    print( len(t_dl) )
+    print( len(e_dl) )
+
+    
+    # Test transformer
+    #_ = transformer.test()
+
+    print('Done')
 
--- ai4code/data/dataloader.py
+++ ai4code/data/dataloader.py
@@ -7,28 +7,22 @@ import numpy as np
 import torch
 import pickle
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from random import shuffle
 
 class consecutive_cells(Dataset):
     '''
     This dataset is going to be slightly different in it will generate "new"
     data after each epoch. 
-
-    TODO:
-        - Making training data list is taking way to long. 
-            Add multi processing to ease burden,
-            Use Queue to store data, 
-            Shuffle items before processing (to add randomness)
-            Just pull head instead of index. 
     '''
     def __init__( self,
                     file:str,
+                    rm_keys:list,
                     num_pos:int=1,
-                    num_neg:int=3,
-                    f16:bool=False):
+                    num_neg:int=3):
         # Load Data
         self.file_name = file
-        self.data = self.load_data()
+        self.data = self.load_data(rm_keys)
     
         # Generate training pairs
         self.num_pos = num_pos
@@ -36,37 +30,31 @@ class consecutive_cells(Dataset):
         if self.num_neg < 1 or self.num_pos < 1:
             raise Exception("num pos/neg needs to be greater than 2 to form a +- pair")
         self.training_data = self.select_data()
+        return
 
-        # Are we training w/ float16 (in on cuda, yes)
-        self.f16 = f16
-
-    def load_data(self)->dict:
+    def load_data(self, rm_keys:list)->dict:
         # Just load Pickel file
         with open(self.file_name, 'rb') as handle:
             data = pickle.load(handle) 
+        # "split" dataset
+        for k in rm_keys: 
+            del data[k]
         return data
 
     def select_data(self)->list:
+        '''
+        Generate pairs (arr_key, input_pos, output_pos, prob)
+        '''
         pair_list = [ None for i in range( len(self.data)*(self.num_pos + self.num_neg))]
         pair_count = 0 # counter for pair_list
-        for _,arr in self.data.items():
-            if (arr.shape[1]!=768):
-                print( arr.shape )
-                raise ValueError("Something went wrong")
-
+        for arr_ID,arr in self.data.items():
             # Generate Positive examples
             for i in range(self.num_pos):
-                r = np.random.randint(0,arr.shape[0])
-                p1 = arr[r,:]
-                p1 = np.reshape(p1, (1,*p1.shape))
-                if r == arr.shape[0]-1:
-                    p2 = np.zeros(p1.shape) #p1 was already reshaped!
-                else:
-                    p2 = arr[r+1,:]
-                    p2 = np.reshape(p2, (1,*p2.shape))
-                X = np.append(p1, p2, axis=0)
-                Y = 1.
-                pair_list[pair_count] = (X,Y)
+                r1 = np.random.randint(0,arr.shape[0])
+                r2 = r1+1
+                if r2 >= arr.shape[0]:
+                    r2= -1 
+                pair_list[pair_count] = (arr_ID, r1, r2, 1.)
                 pair_count += 1
 
             # Generate Negative Examples
@@ -76,20 +64,16 @@ class consecutive_cells(Dataset):
                 r2 = np.random.randint(0,arr.shape[0])  
                 while r1 == r2:
                     r2 = np.random.randint(0,arr.shape[0])  
-                # Pairs
-                p1 = arr[r1,:]
-                p1 = np.reshape(p1, (1,*p1.shape))
-                p2 = arr[r2,:]
-                p2 = np.reshape(p2, (1,*p2.shape))
-                X = np.append(p1, p2, axis=0)
-                Y = 0.
-                pair_list[pair_count] = (X,Y)
+                pair_list[pair_count] = (arr_ID, r1, r2, 0.)
                 pair_count += 1
         return pair_list
 
 
     def __len__(self):
         return len(self.training_data)
+
+    def select_pair(self, pair):
+        pass
 
     def __getitem__(self,index)->tuple:
         '''
@@ -99,14 +83,55 @@ class consecutive_cells(Dataset):
         Returns:
             A tuple in the format (X,y) where X is a 2xSize tensor, y=0/1
         '''
-        nX,nY = self.training_data[index]
-        X = torch.tensor(nX)
-        y = torch.tensor(nY)
+        a_id, p1, p2, prob = self.training_data[ index ]
+        all_arr = self.data[a_id]
+        # Select Data
+        in_x = all_arr[p1]
+        if p2 == -1:
+            out_x = torch.zeros(in_x.shape) 
+        else:
+            out_x = all_arr[p2]
+        return (in_x, out_x, prob)
 
-        if self.f16:
-            X = X.half()
-            y = y.half()
-        return (X,y)
+
+# Could replace loader in class above -- TODO
+def load_pkl_file(file_path:str):
+    with open(file_path, 'rb') as handle:
+        data = pickle.load(handle) 
+    return data
+
+
+def get_dataloaders(emb_file:str, key_file:str, tv_split:float=.7)->tuple:
+    '''
+    Create 2 dataloaders (from the embedding datasets), Eval and a Train.
+    Paritioning the keys random each time they are created
+    Inputs:
+        - emb_file (str): Path to the embedding dict
+        - key_file (str): Path to the key list
+        - tv_split (float): % of keys to use as training data (1-x for eval).
+    Returns:
+        Tuple of training and eval dataloaders
+
+    TODO -- Add more variable passthough
+    '''
+    # Get and partition keys
+    keys = load_pkl_file(key_file)
+    shuffle(keys)
+    print('# keys: {}'.format(len(keys)))
+    x = int(len(keys) * tv_split)
+    e_keys = keys[:x] # If key is present del it from dataset
+    t_keys = keys[x:]
+    print('x({}) t({}) e({})'.format(x,len(t_keys), len(e_keys)))
+
+    # Create Datasets
+    t_ds = consecutive_cells(emb_file, t_keys) 
+    e_ds = consecutive_cells(emb_file, e_keys) 
+
+    # Create Dataloaders
+    t_dl = DataLoader( t_ds, batch_size=64, shuffle=True, num_workers=6,)
+    e_dl = DataLoader( e_ds, batch_size=64, shuffle=True, num_workers=6,)
+
+    return (t_dl, e_dl) 
 
 
 
